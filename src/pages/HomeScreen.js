db0173ddad3e234598a93c644e785add64a073ff
GabrielvfDelgado <gabrielvfdelgado@gmail.com>
2023-01-22 03:31:35 -0300

    Added insert and list in db

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,11 +1,81 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import { DatabaseConnection } from '../database/database_connetion';
+import { ListItem, Button, Icon } from '@rneui/base';
 
+const db = DatabaseConnection.getConnection();
 
 export default props => {
+
+  const isFocused = useIsFocused();
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_task'",
+        [],
+        function (tx, res) {
+          console.log('item:', res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS table_task', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS table_task(task_id INTEGER PRIMARY KEY AUTOINCREMENT, task_name VARCHAR(20), task_duration VARCHAR(8), pomodoro_necessary INT(10), pomodoro_done INT(10), pomodoro_missing INT(10), concluded boolean)',
+              []
+            );
+          }
+        }
+      );
+    });
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM table_task',
+        [],
+        (tx, results) => {
+          const temp = [];
+          for (let i = 0; i < results.rows.length; ++i)
+            temp.push(results.rows.item(i));
+          setItems(temp);
+        }
+      );
+    });
+  }, []);
+
+  function getActions(tarefa) {
+    return (
+      <>
+        <Button
+          type="clear"
+          icon={<Icon name='alarm' size={25} color="orange" />}
+          onPress={() => props.navigation.navigate('TimerScreen')}
+        ></Button>
+      </>
+    );
+  }
+
+  function getTarefaItem({ item: task }) {
+    return (
+
+      <ListItem
+        bottomDivider
+        key={task.task_id}>
+        <ListItem.Content>
+          <ListItem.Title>{task.task_name}</ListItem.Title>
+        </ListItem.Content>
+        {getActions(task)}
+      </ListItem>
+    );
+  }
+
   return (
-    <View>
-      <Text>Home Page</Text>
-    </View>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={items}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={getTarefaItem}
+      ></FlatList>
+    </View >
   );
 };
--- src/pages/RegisterTasks.js
+++ src/pages/RegisterTasks.js
@@ -1,11 +1,85 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View, StyleSheet, Alert } from 'react-native';
+import { Button } from '@rneui/base';
+import { calcularPomodoros } from '../service/data_handler_register';
+import { DatabaseConnection } from '../database/database_connetion';
 
+const db = DatabaseConnection.getConnection();
 
 export default props => {
+  const [taskName, setTaskName] = useState('');
+  const [taskTime, setTaskTime] = useState('');
+
+  const register_task = () => {
+    console.log('primeiro log register: ' + taskName, taskTime);
+
+    if (!taskName) {
+      alert('Por favor preencha o nome!');
+      return;
+    }
+    if (!taskTime) {
+      alert('Por favor preencha o tempo da tarefa');
+      return;
+    }
+
+    const pomodoro_necessary = calcularPomodoros(taskTime);
+
+    db.transaction(function (tx) {
+      tx.executeSql(
+        'INSERT INTO table_task (task_name, task_duration, pomodoro_necessary, pomodoro_done, pomodoro_missing, concluded) VALUES (?,?,?,?,?,?)',
+        [taskName, taskTime, pomodoro_necessary, 0, pomodoro_necessary, false],
+        (tx, results) => {
+          console.log('segundo log register: ' + results.rowsAffected);
+          if (results.rowsAffected > 0) {
+            Alert.alert(
+              'Sucesso',
+              'Usuário Registrado com Sucesso !!!',
+              [
+                {
+                  text: 'Ok',
+                  onPress: () => props.navigation.navigate('HomeScreen'),
+                },
+              ],
+              { cancelable: false }
+            );
+          } else alert('Erro ao tentar Registrar o Usuário !!!');
+        }
+      );
+    });
+
+  };
+
   return (
-    <View>
-      <Text>Register Task</Text>
+    <View style={style.form}>
+      <Text>Nome da tarefa:</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={taskName => setTaskName(taskName)}
+        placeholder='Informe o Nome'
+      />
+      <Text>Tempo da tarefa</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={taskTime => setTaskTime(taskTime)}
+        placeholder='Informe o tempo'
+      />
+      <Button
+        title="Salvar"
+        onPress={register_task}
+      />
     </View>
+
   );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 12
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10
+  }
+});
