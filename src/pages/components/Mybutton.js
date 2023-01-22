062e7e75fd767c2a12779b75965da63c7303c4fa
GabrielvfDelgado <gabrielvfdelgado@gmail.com>
2023-01-22 05:11:32 -0300

    Added components

---
+++ src/pages/components/Mybutton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Mybutton = (props) => {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={props.customClick}>
+
+      <Text style={styles.text}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#439A97',
+    color: '#ffffff',
+    padding: 10,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+    borderRadius: 10,
+    minWidth: '75%'
+  },
+  text: {
+    color: '#ffffff',
+  },
+});
+
+export default Mybutton;
