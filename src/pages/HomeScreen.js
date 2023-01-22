341cd29d2633f5c85075bd896b28640c05e16c92
GabrielvfDelgado <gabrielvfdelgado@gmail.com>
2023-01-22 05:11:59 -0300

    Updated HomeScreen

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -12,36 +12,38 @@ export default props => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    db.transaction(function (txn) {
-      txn.executeSql(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_task'",
-        [],
-        function (tx, res) {
-          console.log('item:', res.rows.length);
-          if (res.rows.length == 0) {
-            txn.executeSql('DROP TABLE IF EXISTS table_task', []);
-            txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS table_task(task_id INTEGER PRIMARY KEY AUTOINCREMENT, task_name VARCHAR(20), task_duration VARCHAR(8), pomodoro_necessary INT(10), pomodoro_done INT(10), pomodoro_missing INT(10), concluded boolean)',
-              []
-            );
+    if (isFocused) {
+      db.transaction(function (txn) {
+        txn.executeSql(
+          "SELECT name FROM sqlite_master WHERE type='table' AND name='table_task'",
+          [],
+          function (tx, res) {
+            console.log('item:', res.rows.length);
+            if (res.rows.length == 0) {
+              txn.executeSql('DROP TABLE IF EXISTS table_task', []);
+              txn.executeSql(
+                'CREATE TABLE IF NOT EXISTS table_task(task_id INTEGER PRIMARY KEY AUTOINCREMENT, task_name VARCHAR(20), task_duration VARCHAR(8), pomodoro_necessary INT(10), pomodoro_done INT(10), pomodoro_missing INT(10), concluded boolean)',
+                []
+              );
+            }
           }
-        }
-      );
-    });
+        );
+      });
 
-    db.transaction((tx) => {
-      tx.executeSql(
-        'SELECT * FROM table_task',
-        [],
-        (tx, results) => {
-          const temp = [];
-          for (let i = 0; i < results.rows.length; ++i)
-            temp.push(results.rows.item(i));
-          setItems(temp);
-        }
-      );
-    });
-  }, []);
+      db.transaction((tx) => {
+        tx.executeSql(
+          'SELECT * FROM table_task',
+          [],
+          (tx, results) => {
+            const temp = [];
+            for (let i = 0; i < results.rows.length; ++i)
+              temp.push(results.rows.item(i));
+            setItems(temp);
+          }
+        );
+      });
+    }
+  }, [props, isFocused]);
 
   function getActions(tarefa) {
     return (
